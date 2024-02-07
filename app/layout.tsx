74b69a9316b164e572a8087d3d76fa55fa3aa001
Ryan Dawes <dev@dawes.cc>
2024-02-06 22:45:52 -0500

    start point

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,40 @@
-import { GeistSans } from "geist/font/sans";
+import { Be_Vietnam_Pro as FontSans } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/ThemeProvider";
+import { Toaster } from "sonner";
 
-const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
+const defaultUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000";
 
 export const metadata = {
-  metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+	metadataBase: new URL(defaultUrl),
+	title: "Next.js and Supabase Starter Kit",
+	description: "The fastest way to build apps with Next.js and Supabase",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en" className={GeistSans.className}>
-      <body className="bg-background text-foreground">
-        <main className="min-h-screen flex flex-col items-center">
-          {children}
-        </main>
-      </body>
-    </html>
-  );
+export const fontSans = FontSans({
+	subsets: ["latin"],
+	weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+	variable: "--font-sans",
+});
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+	return (
+		<html suppressHydrationWarning>
+			<body className={cn("min-h-dvh h-dvh bg-background text-foreground font-sans antialiased", fontSans.variable)}>
+				<ThemeProvider
+					attribute='class'
+					defaultTheme='system'
+					enableSystem
+					disableTransitionOnChange>
+					{children}
+					<Toaster
+						closeButton
+						position='top-right'
+						expand={false}
+					/>
+				</ThemeProvider>
+			</body>
+		</html>
+	);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,57 +1,49 @@
-import DeployButton from "../components/DeployButton";
+import { ModeToggle } from "@/components/ModeToggle";
 import AuthButton from "../components/AuthButton";
 import { createClient } from "@/utils/supabase/server";
-import ConnectSupabaseSteps from "@/components/ConnectSupabaseSteps";
-import SignUpUserSteps from "@/components/SignUpUserSteps";
-import Header from "@/components/Header";
+import { HeartHandshake } from "lucide-react";
 import { cookies } from "next/headers";
 
 export default async function Index() {
-  const cookieStore = cookies();
+	const cookieStore = cookies();
 
-  const canInitSupabaseClient = () => {
-    // This function is just for the interactive tutorial.
-    // Feel free to remove it once you have Supabase connected.
-    try {
-      createClient(cookieStore);
-      return true;
-    } catch (e) {
-      return false;
-    }
-  };
+	const canInitSupabaseClient = () => {
+		// This function is just for the interactive tutorial.
+		// Feel free to remove it once you have Supabase connected.
+		try {
+			createClient(cookieStore);
+			return true;
+		} catch (e) {
+			return false;
+		}
+	};
 
-  const isSupabaseConnected = canInitSupabaseClient();
+	const isSupabaseConnected = canInitSupabaseClient();
 
-  return (
-    <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-        <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-          <DeployButton />
-          {isSupabaseConnected && <AuthButton />}
-        </div>
-      </nav>
+	return (
+		<div className='h-full w-full flex flex-col'>
+			<nav className='w-full flex justify-center h-16 border-b border-b-border'>
+				<div className='w-full max-w-4xl flex justify-between items-center p-3 text-sm'>
+					<AuthButton />
+					<ModeToggle />
+				</div>
+			</nav>
 
-      <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
-        <Header />
-        <main className="flex-1 flex flex-col gap-6">
-          <h2 className="font-bold text-4xl mb-4">Next steps</h2>
-          {isSupabaseConnected ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-        </main>
-      </div>
+			<main className='flex-1 flex flex-col gap-6 px-4 py-10'>
+				<h2 className='font-bold text-4xl mb-4'>Hi</h2>
+				<div className='flex flex-col gap-4'>
+					{Array.from({ length: 420 }).map((_, index) => (
+						<h3 className='font-bold text-2xl'>Hello for the {index + 1}th time</h3>
+					))}
+				</div>
+			</main>
 
-      <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
-        <p>
-          Powered by{" "}
-          <a
-            href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-            target="_blank"
-            className="font-bold hover:underline"
-            rel="noreferrer"
-          >
-            Supabase
-          </a>
-        </p>
-      </footer>
-    </div>
-  );
+			<footer className='w-full border-t border-t-border p-8 flex justify-center text-center text-xs'>
+				<span className='bg-muted font-semibold select-none inline-flex items-center ring-1 ring-inset ring-border py-1.5 px-2.5 rounded-lg shadow-sm'>
+					Made with &nbsp;
+					<HeartHandshake />
+				</span>
+			</footer>
+		</div>
+	);
 }
