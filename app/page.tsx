f40df61e6edfa04ce149d3253866cd8b2452f32c
Ryan Dawes <dev@dawes.cc>
2024-02-07 00:51:10 -0500

    inclusions

--- app/page.tsx
+++ app/page.tsx
@@ -4,22 +4,13 @@ import { createClient } from "@/utils/supabase/server";
 import { HeartHandshake } from "lucide-react";
 import { cookies } from "next/headers";
 
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
+
+const totalCards = 420;
+
 export default async function Index() {
-	const cookieStore = cookies();
-
-	const canInitSupabaseClient = () => {
-		// This function is just for the interactive tutorial.
-		// Feel free to remove it once you have Supabase connected.
-		try {
-			createClient(cookieStore);
-			return true;
-		} catch (e) {
-			return false;
-		}
-	};
-
-	const isSupabaseConnected = canInitSupabaseClient();
-
 	return (
 		<div className='h-full w-full flex flex-col'>
 			<nav className='w-full flex justify-center h-16 border-b border-b-border'>
@@ -32,8 +23,21 @@ export default async function Index() {
 			<main className='flex-1 flex flex-col gap-6 px-4 py-10'>
 				<h2 className='font-bold text-4xl mb-4'>Hi</h2>
 				<div className='flex flex-col gap-4'>
-					{Array.from({ length: 420 }).map((_, index) => (
-						<h3 className='font-bold text-2xl'>Hello for the {index + 1}th time</h3>
+					{Array.from({ length: totalCards }).map((_, index) => (
+						<Card key={index}>
+							<CardHeader>
+								<CardTitle>Card Number {index + 1}</CardTitle>
+								<CardDescription>Hey, what's up {index + 1}</CardDescription>
+							</CardHeader>
+							<CardContent>
+								<p>
+									This is card number {index + 1} out of {totalCards}
+								</p>
+							</CardContent>
+							<CardFooter>
+								<Button>{index + 1}</Button>
+							</CardFooter>
+						</Card>
 					))}
 				</div>
 			</main>
