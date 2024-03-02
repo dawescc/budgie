8171ced2dcc81d761f471a0bd4723a3dd154348b
Ryan Dawes <dev@dawes.cc>
2024-03-02 01:13:48 -0500

    added styles

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,39 +2,43 @@ import { signIn } from "@/components/actions";
 
 export default function Login({ searchParams }: { searchParams: { message: string } }) {
 	return (
-		<div className=''>
+		<div className='h-dvh w-full flex items-center transition-all'>
 			<form
-				className=''
+				className='m-auto w-full max-w-md flex flex-col gap-8'
 				action={signIn}>
-				<label
-					className=''
-					htmlFor='email'>
-					Email
-				</label>
-				<input
-					className=''
-					name='email'
-					autoComplete='email'
-					id='email'
-					placeholder='you@example.com'
-					required
-				/>
-				<label
-					className=''
-					htmlFor='password'>
-					Password
-				</label>
-				<input
-					className=''
-					type='password'
-					id='password'
-					autoComplete='password'
-					name='password'
-					placeholder='••••••••'
-					required
-				/>
-				<button>Sign In</button>
+				<div className='flex flex-col gap-2'>
+					<label
+						className=''
+						htmlFor='email'>
+						Email
+					</label>
+					<input
+						className=''
+						name='email'
+						autoComplete='email'
+						id='email'
+						placeholder='you@example.com'
+						required
+					/>
+				</div>
+				<div className='flex flex-col gap-2'>
+					<label
+						className=''
+						htmlFor='password'>
+						Password
+					</label>
+					<input
+						className=''
+						type='password'
+						id='password'
+						autoComplete='password'
+						name='password'
+						placeholder='••••••••'
+						required
+					/>
+				</div>
 				{searchParams?.message && <p className=''>{searchParams.message}</p>}
+				<button disabled>Sign In</button>
 			</form>
 		</div>
 	);
